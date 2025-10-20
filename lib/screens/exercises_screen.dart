23a4adc98df234c553622fc80e6e23559bfaea52
kabe <kabevb@gmail.com>
2025-10-20 22:08:50 +0000

    Oct 21, 2025, 12:08 AM

--- lib/screens/exercises_screen.dart
+++ lib/screens/exercises_screen.dart
@@ -165,7 +165,7 @@ class _ExercisesScreenState extends State<ExercisesScreen> {
         top: true,
         bottom: false,
         child: CustomScrollView(
-            slivers: [
+          slivers: [
           // Header
           SliverToBoxAdapter(
             child: Padding(
